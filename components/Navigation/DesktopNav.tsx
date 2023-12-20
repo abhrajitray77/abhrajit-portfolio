27f8ea864057bd9ef54d035fab88fcd24f3d6872
abhrajitray77 <abhrajitray77@gmail.com>
2023-12-20 11:55:01 +0530

    🎨 styling improments

--- components/Navigation/DesktopNav.tsx
+++ components/Navigation/DesktopNav.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const DesktopNav = () => {
   return (
-    <nav className='border-[1px] border-neutral-900 py-4 px-10'>
+    <nav className='border-[1px] border-neutral-900 bg-neutral-100 py-4 px-10 lg:px-20'>
         <ul className='flex justify-between space-x-10 uppercase'>
-           <li>
+           <li className="nav-item">
                 About Me
             </li> 
-            <li>
+            <li className="nav-item">
                 Projects
             </li>
-            <li>
+            <li className="nav-item">
                 Blog
             </li>
-            <li>
+            <li className="nav-item">
                 Contact
             </li>
         </ul>
