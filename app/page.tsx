7b07761f1bc59c9d790ffa38a49067086c6ade65
abhrajitray77 <abhrajitray77@gmail.com>
2023-12-20 13:09:44 +0530

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="">
       {/* Hero section */}
-      <section className='max-w-6xl xl:max-w-7xl mx-auto px-4 lg:px-10 py-4'>
+      <section className='max-w-6xl xl:max-w-7xl mx-auto px-4 md:px-10 py-4'>
         <Hero />
       </section>
     </main>
