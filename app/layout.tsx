106f21aefc854fc83839c7add55af27b15d93f6b
abhrajitray77 <abhrajitray77@gmail.com>
2024-01-20 00:22:53 +0530

    🎫 Changed Hero layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,20 @@ import "./styles/bg-patterns.css";
 import Header from "@/components/Header";
 import SideStrips from "@/components/SideStrips";
 import { Providers } from "./Providers";
-import { futureCondensed, futureHalftone, futureHalftoneItalic, futureItalic, futureNormal, futureSemiItalic } from "./fonts";
+import {
+  futureCondensed,
+  futureHalftone,
+  futureHalftoneItalic,
+  futureItalic,
+  futureNormal,
+  futureSemiItalic,
+} from "./fonts";
 import LeftSide from "@/components/LeftSide";
 import RightSide from "@/components/RightSide";
 
 const inter = Inter({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Abhrajit Ray",
@@ -25,9 +32,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${futureNormal.variable} ${futureCondensed.variable}
+      <body
+        className={`${inter.className} ${futureNormal.variable} ${futureCondensed.variable}
       ${futureHalftone.variable} ${futureHalftoneItalic.variable} ${futureSemiItalic.variable}
-      ${futureItalic.variable}`}>
+      ${futureItalic.variable}`}
+      >
         <Providers>
           {/* background patterns */}
           <div className="fixed inset-0 h-full w-full grid-bg -z-50"></div>
@@ -39,22 +48,10 @@ export default function RootLayout({
             className="fixed -z-50 bottom-10 right-10 w-72 h-72 bg-[#afbaec]
         rounded-full blur-3xl opacity-50"
           ></div>
-          <div className="flex">
-            {/* left */}
-            <div className="hidden md:block">
-              <LeftSide />
-            </div>
-            <div className="flex-1">
-              <header>
-                <Header />
-              </header>
-              {children}
-            </div>
-            {/* right */}
-            <div className="hidden md:block">
-              <RightSide />
-            </div>
-          </div>
+          <header>
+            <Header />
+          </header>
+          {children}
         </Providers>
       </body>
     </html>
--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -2,19 +2,17 @@ import React from 'react'
 
 const RightSide = () => {
   return (
-    <div className="flex flex-col my-10 h-[90%] md:w-fit mx-2 diag-lines">
-    {/* Hello text */}
-    <div className="mt-auto bg-neutral-100 p-2
-    w-fit rotate-180"
-    style={{ writingMode: 'vertical-rl' }}>
-        <h1 className="text-2xl lg:text-6xl font-bold text-neutral-800
-        border-[1px] border-neutral-900 px-4 py-2 ">
-            HELLO!
-        </h1>
+    <div className="flex flex-col h-full md:w-fit mx-1 md:mx-2 diag-lines">
+        {/* Hello text */}
+        <div className="mt-auto bg-neutral-100 px-1 lg:px-2 lg:py-2 pt-6
+        w-fit"
+        style={{ writingMode: 'vertical-rl' }}>
+            <h3 className="text-2xl lg:text-6xl font-bold text-neutral-800
+            border-[1px] border-neutral-900 pl-1 lg:pl-2 pr-2 lg:pr-4 py-2">
+                こんにちは
+            </h3>
+        </div>
     </div>
-    
-    
-</div>
   )
 }
 
