63b473d59261063a7dceb57e01264a5541beba54
abhrajitray77 <abhrajitray77@gmail.com>
2024-01-21 21:37:09 +0530

    📰 Getting dynamic post data in article page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { getClient } from "@/app/lib/graphQlClient";
+import { getPost } from "@/app/lib/queries";
+import { BlogPostData } from "@/types/blogTypes";
+import { notFound } from "next/navigation";
+import React from "react";
 
-const page = () => {
-  return (
-    <div>Article page</div>
-  )
+type PostProps = {
+  params: {
+    slug: string;
+  };
+};
+
+// this function is used to generate static paths for the dynamic routes
+export async function generateStaticParams() {
+  const client = getClient();
+  const { data } = await client.query({ query: getPost });
+  return data?.me?.posts?.nodes?.map((post: BlogPostData) => ({
+    slug: post?.slug,
+  }));
 }
 
-export default page
+const PostPage = async ({ params }: PostProps) => {
+  // checking if the post exists and if not, return 404
+  const client = getClient();
+  const { data } = await client.query({ query: getPost });
+  const post = data?.me?.posts?.nodes?.find(
+    (post: BlogPostData) => post?.slug === params?.slug
+  );
+  if (!post) return notFound();
+
+  return (
+    <article>
+      <h1>{post?.title}</h1>
+      <p>{post?.brief}</p>
+    </article>
+  );
+};
+
+export default PostPage;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { getClient } from '../lib/graphQlClient';
-import { getPost } from '../lib/queries';
-import BlogCard from './components/BlogCard';
+import React from "react";
+import { getClient } from "../lib/graphQlClient";
+import { getPost } from "../lib/queries";
+import BlogCard from "./components/BlogCard";
+import Link from "next/link";
+import { BlogPostData } from "@/types/blogTypes";
 
 const blog = async () => {
-    const client = getClient();
-    const {data} = await client.query({ query: getPost });
-    //console.log(data.me.posts);
+  const client = getClient();
+  const { data } = await client.query({ query: getPost });
+  //console.log(data.me.posts.nodes);
 
   return (
     <div className="max-w-6xl mx-auto px-4 lg:px-10 py-4">
-        <h1 className="flex justify-center text-4xl font-semibold text-neutral-700 mb-4 uppercase">
-          Blog
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {
-            data.me.posts.nodes.map((post:any, index: number) => {
-              return (
-                <BlogCard key={index} post={post} />
-              )
-            })
-          }
-        </div>
-
+      <h1 className="flex justify-center text-4xl font-semibold text-neutral-700 mb-4 uppercase">
+        Blog
+      </h1>
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {data?.me?.posts?.nodes?.map((post: BlogPostData, index: number) => {
+          return (
+            <li key={index}>
+              <Link
+                href={`/blog/${post?.slug}`}
+                aria-label={`${post?.title} ${post?.brief} ${post?.publishedAt}`}
+              >
+                <BlogCard post={post} />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default blog
+export default blog;
